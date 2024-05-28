5a4d6b9f873f2c7329b778b0b2d01ec4d5336bbc
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-29 05:20:36 +0530

    UPdate
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignIn from "./components/LoginSignUp/SignIn";
+import SignUp from "./components/LoginSignUp/SignUp";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        
+        <Route path="/" element={<SignIn />} />
+        <Route path="/SignUp" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   )
