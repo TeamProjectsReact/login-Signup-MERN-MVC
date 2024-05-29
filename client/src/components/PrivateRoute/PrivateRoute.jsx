42ddd913b8577aa7bfdc53330170c5f0fff1eb5b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-29 14:09:47 +0530

    Create PrivateRoute.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

---
+++ client/src/components/PrivateRoute/PrivateRoute.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import axios from 'axios';
+import  secureLocalStorage  from  "react-secure-storage";
+
+const PrivateRoute = () => {
+  return (
+    <div>PrivateRoute</div>
+  )
+}
+
+export default PrivateRoute
