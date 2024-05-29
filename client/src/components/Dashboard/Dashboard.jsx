06edc6c5a4890abd44ad624c0202d30a23930975
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-29 14:08:55 +0530

    Update Dashboard.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import axios from 'axios';
+import  secureLocalStorage  from  "react-secure-storage";
 
 const Dashboard = () => {
+    
+    
   return (
     <div>Dashboard</div>
   )
