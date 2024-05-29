41351640a0a97a23c70e18bfb82fcb58406429b2
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-29 22:22:37 +0530

    Update StudentController.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/Controllers/StudentController.js
+++ server/Controllers/StudentController.js
@@ -2,9 +2,23 @@ const multer = require('multer')
 const path = require('path')
 const Student = require('../Models/Student')
 
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+});
+
+const upload = multer({ storage }).single('image');
+
+
 const StudentController = {
+
     AddStundet: async (req, res) => {
-        
+
     },
 }
 
