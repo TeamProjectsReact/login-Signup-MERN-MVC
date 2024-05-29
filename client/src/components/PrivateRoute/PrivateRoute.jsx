d3d4e87781ecd14a89a6482ff577d5b4ddba41c4
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-29 14:20:13 +0530

    Update PrivateRoute.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/PrivateRoute/PrivateRoute.jsx
+++ client/src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import axios from 'axios';
 import  secureLocalStorage  from  "react-secure-storage";
+import { useNavigate } from 'react-router-dom';
 
 const PrivateRoute = ({ ProtectRoute }) => {
-    const RoleUser = secureLocalStorage.getItem("Login1");
-    const EmailUser = secureLocalStorage.getItem("login2");
+    const navigate = useNavigate()
+    const EmailUser = secureLocalStorage.getItem("Login1");
+    const RoleUser = secureLocalStorage.getItem("login2");
+
+    if(EmailUser !== null && RoleUser !== null){
+        return (
+            ProtectRoute
+        )
+    }
+    else{
+        useEffect(() => {
+            localStorage.clear()
+            navigate('/')
+        }, [])
+    }
 }
 
 export default PrivateRoute
