45ef13f32a93c78525fd8a9df5bd65fd9d0e774c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-29 06:43:36 +0530

    Update User.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/Models/User.js
+++ server/Models/User.js
@@ -18,6 +18,10 @@ const userSchema = new mongoose.Schema({
     Role: {
         type: String,
         required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
     }    
 })
 
