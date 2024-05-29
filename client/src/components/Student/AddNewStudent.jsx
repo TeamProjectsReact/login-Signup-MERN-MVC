c46a3b902227afd0d2b2f45bf1ac94e740bb4d69
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-29 22:27:37 +0530

    Create AddNewStudent.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

---
+++ client/src/components/Student/AddNewStudent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import axios from 'axios';
+import  secureLocalStorage  from  "react-secure-storage";
+import { useNavigate } from 'react-router-dom';
+
+const AddNewStudent = () => {
+    const navigate = useNavigate()
+
+  return (
+    <div>AddNewStudent</div>
+  )
+}
+
+export default AddNewStudent
