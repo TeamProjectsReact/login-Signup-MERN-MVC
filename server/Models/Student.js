acb33d0229fd33ce45667f6a3f11ab2b0c70abdd
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-29 22:08:30 +0530

    Create Student.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

---
+++ server/Models/Student.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const studentSchema = new mongoose.Schema({
+    RegID: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    NIC: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    Fname: {
+        type: String,
+        require: true,
+    },
+    Lname: {
+        type: String,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }   
+
+
+
+})
