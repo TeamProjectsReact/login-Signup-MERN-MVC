a3ba505fb9c13ba942684af7383b405c0faa6baf
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-30 05:54:11 +0530

    Update Students.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Student/Students.jsx
+++ client/src/components/Student/Students.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios';
 import  secureLocalStorage  from  "react-secure-storage";
 import { useNavigate } from 'react-router-dom';
 
 const Students = () => {
+    // student data
+    const [stdData, SetStdData] = useState({
+        RegID: '',
+        fname: '',
+        lname: '',
+    })
   return (
     <div className='bg-gray-200 py-16 md:px-20 px-8 min-h-screen'>
         <div className="bg-white py-8 md:px-12 px-4 rounded shadow-md">
             <h1 className="text-xl font-semibold text-gray-500">Add New Student</h1>
         </div>
         <div className="bg-white py-8 md:px-12 px-4 rounded shadow-md my-4">
-            <h1 className="text-xl font-semibold text-gray-500">Dashboard</h1>
+            <p className="text-gray-500"></p>
         </div>
 
     </div>
