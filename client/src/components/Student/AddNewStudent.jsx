fd5edb104d27a80c9facc7e4a06072f0e4f46bbf
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-30 06:12:06 +0530

    Update AddNewStudent.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Student/AddNewStudent.jsx
+++ client/src/components/Student/AddNewStudent.jsx
@@ -9,10 +9,20 @@ const AddNewStudent = () => {
     // student data
     const [stdData, SetStdData] = useState({
         RegID: '',
+        NIC: '',
         fname: '',
         lname: '',
         image: null
     })
+
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+    const handleFileChange = (e) => {
+        setFormData({ ...formData, image: e.target.files[0] });
+    };
 
   return (
     <div className='bg-gray-200 py-16 md:px-20 px-8 min-h-screen'>
@@ -24,12 +34,30 @@ const AddNewStudent = () => {
         </div>
         <div className="bg-white py-8 md:px-12 px-4 rounded shadow-md my-4">
             <div className="text-gray-500">
-                <div className="md:grid grid-cols-3 gap-5">
-                    <div className="">
-                        <label htmlFor="">Registation Number: </label>
-                        <input type="text" name="regNo" id="regNo" className="w-full h-12 rounded bg-gray-200 pl-2 my-2"  required placeholder='Enter Regisatation Number'/>
+                <form action="">
+                    <div className="md:grid grid-cols-3 gap-5">
+                        <div className="">
+                            <label htmlFor="">Registation Number: </label>
+                            <input type="text" name="regNo" id="regNo" className="w-full h-12 rounded bg-gray-200 pl-2 my-2"  required placeholder='Enter Regisatation Number'
+                            onChange={handleInputChange} value={formData.RegID}/>
+                        </div>
+                        <div className="">
+                            <label htmlFor="">NIC Number: </label>
+                            <input type="text" name="NIC" id="NIC" className="w-full h-12 rounded bg-gray-200 pl-2 my-2"  required placeholder='Enter NIC Number'
+                            onChange={handleInputChange} value={formData.NIC}/>
+                        </div>
+                        <div className="">
+                            <label htmlFor="">First Name : </label>
+                            <input type="text" name="regNo" id="regNo" className="w-full h-12 rounded bg-gray-200 pl-2 my-2"  required placeholder='Enter First Name'
+                            onChange={handleInputChange} value={formData.fname}/>
+                        </div>
+                        <div className="">
+                            <label htmlFor="">Last Name : </label>
+                            <input type="text" name="regNo" id="regNo" className="w-full h-12 rounded bg-gray-200 pl-2 my-2"  required placeholder='Enter Last Name'
+                            onChange={handleInputChange} value={formData.lname}/>
+                        </div>
                     </div>
-                </div>
+                </form>
             </div>
         </div>
     </div>
