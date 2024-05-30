d1fa5d0d057146de13a0616f84f5a8dfb060ecde
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-30 05:52:40 +0530

    Update Students.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Student/Students.jsx
+++ client/src/components/Student/Students.jsx
@@ -7,8 +7,12 @@ const Students = () => {
   return (
     <div className='bg-gray-200 py-16 md:px-20 px-8 min-h-screen'>
         <div className="bg-white py-8 md:px-12 px-4 rounded shadow-md">
+            <h1 className="text-xl font-semibold text-gray-500">Add New Student</h1>
+        </div>
+        <div className="bg-white py-8 md:px-12 px-4 rounded shadow-md my-4">
             <h1 className="text-xl font-semibold text-gray-500">Dashboard</h1>
         </div>
+
     </div>
   )
 }
