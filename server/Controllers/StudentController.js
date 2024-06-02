e529fe006328dc71e5e5742359ccbdf9e8bd6b69
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-02 17:06:17 +0530

    Update StudentController.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/Controllers/StudentController.js
+++ server/Controllers/StudentController.js
@@ -3,6 +3,17 @@ const path = require('path')
 
 const StudentController = {
     AddStudent: async (req, res) => {
+        const { title } = req.body;
+        const imageUrl = req.file.path;
+
+        const newImage = new Students({
+            title,
+            imageUrl,
+        });
+
+        await newImage.save();
+
+        return res.json({Status: "Success"})
         
     }
 }
