26656a7a4b3d91c270e88df6f5c5d4713a7d3996
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-02 16:56:35 +0530

    Update Student.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/Models/Student.js
+++ server/Models/Student.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose');
 
 const StudentSchema = new mongoose.Schema({
-
+    title: {
+        type: String,
+        required: true,
+      },
+    imageUrl: {
+        type: String,
+        required: true,
+    },
 })
 
 const Students = mongoose.model('Students', StudentSchema)
