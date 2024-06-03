d21b8bce8815f51b540752d1e4de7d57ca609d54
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-03 10:49:21 +0530

    Update ImageAlbum.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/TestingImages/ImageAlbum.jsx
+++ client/src/components/TestingImages/ImageAlbum.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import axios from 'axios';
+import  secureLocalStorage  from  "react-secure-storage";
+import { Link, useNavigate } from 'react-router-dom';
 
 const ImageAlbum = () => {
+    const navigate = useNavigate()
+    const EmailUser = secureLocalStorage.getItem("Login1");
+    const RoleUser = secureLocalStorage.getItem("Login2");
+
+    
   return (
-    <div>ImageAlbum</div>
+    <div className='bg-gray-200 py-8 px-12'>
+        <div className="bg-white py-8 px-12 rounded shadow-md">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quisquam quis eaque necessitatibus. Itaque placeat doloribus tenetur qui nemo fugit cupiditate. Officia ab quidem ex aspernatur harum ut quasi illo.
+        </div>
+    </div>
   )
 }
 
