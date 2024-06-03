21d8a1cfaadfdcb7b1abd5ae868f2ea08e47c25e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-03 11:14:57 +0530

    Update TextArea.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/TestArea/TextArea.jsx
+++ client/src/components/TestArea/TextArea.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const TextArea = () => {
   return (
-    <div>TextArea</div>
+    <div className='bg-gray-200 py-8 px-12 min-h-screen'>
+        <div className="bg-white py-4 px-8 rounded shadow-md">
+            lorem
+        </div>
+    </div>
   )
 }
 
