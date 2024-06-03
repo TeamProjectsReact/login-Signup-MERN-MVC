d1c250bdeb4c55611d2bfefe031abbd4110d936d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-03 11:35:28 +0530

    Update Employee.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/Models/Employee.js
+++ server/Models/Employee.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
 
 const EmployeeSchema = new mongoose.Schema({
-    
+    content: {
+        type: String,
+        required: true,
+    },
 })
 
 const Employee = mongoose.model('Employee', EmployeeSchema)
