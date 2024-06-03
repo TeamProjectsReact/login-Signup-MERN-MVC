ba2c3a32492f464df300ec619e6a14c1306dd7c8
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-03 11:38:58 +0530

    Update EmployeeController.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/Controllers/EmployeeController.js
+++ server/Controllers/EmployeeController.js
@@ -4,7 +4,15 @@ const EmployeeController = {
     AddNewData: async (req, res) => {
         const { dataUser } = req.body
 
-        
+        // save data
+        const NewData = new Employee({ content: dataUser })
+
+        if(NewData){
+            return res.json({Status: "Success"})
+        }
+        else{
+            return res.json({Error: "Internal Server Error"})
+        }
     }
 }
 
